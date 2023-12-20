ab5f45a1cfbe7cffa417b85a6a4e6a572505c03e
NightSkumbry <119004283+NightSkumbry@users.noreply.github.com>
2023-12-20 09:23:27 +0700

    Update 12257.py

--- kompege/12257.py
+++ kompege/12257.py
@@ -9,16 +9,16 @@ for i in range(len(a)):
             res = max(res, len(a[i:j]))
 print(res)
 
-with open('27-B_12257.txt') as f:
+with open('27-A_12257.txt') as f:
     n = int(f.readline())
     a = [int(x) for x in f.readlines()]
 k = 113
 c = [0]*(len(a) + 1)
 for i in range(1, len(c)):
     c[i] = a[i-1] + c[i-1]
-b = list(map(lambda x: x % k, c[1:]))
+b = list(map(lambda x: x % k, c[:]))
 g = []
-for i in range(57):
-    g.append(len(b) - b[::-1].index(i) - b.index(i))
+for i in range(k):
+    g.append(len(b) - b[::-1].index(i) - b.index(i) - 1)
 print(max(g))
     
